bf17b2eeb368a7b65b15c1affd1bbbdb603356ba
Nathanael Gethers <nathanael.gethers@premisehealth.com>
2016-04-12 08:21:45 -0500

    added cell xib

--- Paddle/PlayersListVC.swift
+++ Paddle/PlayersListVC.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class PlayersListVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
-    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet var tableView: UITableView!
     
     var players = [Player]()
     let items = ["Item 1", "Item2", "Item3", "Item4"]
@@ -27,6 +27,7 @@ class PlayersListVC: UIViewController, UITableViewDataSource, UITableViewDelegat
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.registerNib(UINib(nibName: "PlayerCell", bundle: nil), forCellReuseIdentifier: "PlayerCell")
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -35,14 +36,27 @@ class PlayersListVC: UIViewController, UITableViewDataSource, UITableViewDelegat
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        if let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell") as? PlayerCell{
+//        if let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell") as? PlayerCell{
 //            let currentPlayers = getPaddlePlayers()
 //            cell.textLabel?.text = currentPlayers[indexPath.row].name
-            cell.textLabel?.text = items[indexPath.row]
+//            cell.textLabel?.text = items[indexPath.row]
+//            cell.configureCell(items[indexPath.row])
+//            return cell
+//        } else {
+//            return PlayerCell()
+//        }
+        
+        if let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell", forIndexPath: indexPath) as? PlayerCell {
+            let players = getPaddlePlayers()
+            cell.configureCell(players![indexPath.row].name)
             return cell
+
         } else {
             return PlayerCell()
         }
+
+//        cell!.configureCell(items[indexPath.row])
+//        return cell!
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -54,7 +68,7 @@ class PlayersListVC: UIViewController, UITableViewDataSource, UITableViewDelegat
         // Dispose of any resources that can be recreated.
     }
     
-    func getPaddlePlayers() -> [Player] {
+    func getPaddlePlayers() -> [Player]? {
         let url = NSURL(string: "http://localhost:3000/players")!
         let session = NSURLSession.sharedSession()
         
